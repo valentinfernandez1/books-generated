0d682bcf647e368d062244dd524c1979aad53e5f
Valentin Fernandez <tinchofernandez8@gmail.com>
2022-10-20 17:03:02 -0300

    prueba 2

--- app.js
+++ app.js
@@ -12,25 +12,28 @@ const port = process.env.PORT || 3000;
 app.set("port", port);
 
 // Middlewares
-app.use( express.json() );
-app.use(logger('dev'));
-app.use( express.urlencoded({ extended: false }) );
-app.use( Cors() );
-
+app.use(express.json());
+app.use(logger("dev"));
+app.use(express.urlencoded({ extended: false }));
+app.use(Cors());
 
 //Routes
-app.get("/", (req,res) => res.sendStatus(200) );
+app.get("/", (req, res) => res.sendStatus(200));
 
 //UtilityRoutes
-app.get('/health', (req,res,next)=> {
-  res.status(200); 
-  res.send('ok')
-})
+app.get("/health", (req, res, next) => {
+  res.status(200);
+  res.send("ok");
+});
 
+//UtilityRoutes
+app.get("/ready", (req, res, next) => {
+  res.status(200);
+  res.send("ok");
+});
 //EntityRoutes
 for (const route of routes.entityRoutes) {
   app.use("/api/entity", route);
 }
-
 
 export default app;
